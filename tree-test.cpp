f6332c7bd71c87702e6f502928b732a7628d40f0
Carl Aslund <caslund@g.hmc.edu>
2018-03-12 15:11:59 -0700

    Add tests for insertion, size, and height

--- tree-test.cpp
+++ tree-test.cpp
@@ -1,14 +1,20 @@
 #include "tree-234.hpp"
 #include "testing-logger.hpp"
 
+#include <iostream>
+
 using namespace std;
 
 /** \brief Test default constructor for TreeString Set
  */
 bool defaultConstructorTest()
 {
-    // TODO: Add tests for the default constructor
     TestingLogger log("default");
+
+    Tree234Set myTree;
+
+    affirm(myTree.size() == 0);
+    affirm(myTree.height() == -1);
 
     // Print a summary of the all the affirmations and return true
     // if they were all successful.
@@ -16,12 +22,39 @@ bool defaultConstructorTest()
 }
 
 /** \brief Test insert and size
- *  \detail TODO: Add details
+ *  \detail Run a test by inserting elements repeatedly into a tree and then
+ *          checking that it worked with size.
  */
 bool insertSizeTest()
 {
-    // TODO: Add tests for insert() and size()
     TestingLogger log("insertSize");
+
+    Tree234Set myTree;
+    affirm(myTree.size() == 0);
+
+    myTree.insert("hello");
+    affirm(myTree.size() == 1);
+
+    myTree.insert("world");
+    affirm(myTree.size() == 2);
+
+    myTree.insert("testing");
+    affirm(myTree.size() == 3);
+
+    myTree.insert("abra");
+    affirm(myTree.size() == 4);
+
+    myTree.insert("world");
+    affirm(myTree.size() == 4);
+
+    myTree.insert("kadabra");
+    affirm(myTree.size() == 5);
+
+    myTree.insert("black");
+    affirm(myTree.size() == 6);
+
+    myTree.insert("panther");
+    affirm(myTree.size() == 7);
 
     // Print a summary of the all the affirmations and return true
     // if they were all successful.
@@ -33,8 +66,46 @@ bool insertSizeTest()
  */
  bool heightTest()
  {
-    // TODO: Add tests for height
     TestingLogger log("height");
+
+    Tree234Set myTree;
+    affirm(myTree.height() == -1);
+
+    myTree.insert("hello");
+    affirm(myTree.height() == 0);
+
+    myTree.insert("world");
+    affirm(myTree.height() == 0);
+
+    myTree.insert("testing");
+    affirm(myTree.height() == 0);
+
+    myTree.insert("abra");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("world");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("kadabra");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("black");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("panther");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("apple");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("banana");
+    affirm(myTree.height() == 1);
+
+    myTree.insert("pear");
+    affirm(myTree.height() == 2);
+
+    // myTree.showStatistics(cout);
+    cout << endl;
 
     //Print a summary of the all the affirmations and return true
     // if they were all successful.
